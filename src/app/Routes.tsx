4be14aba1408a4f385fe60e1f4a9391a440fe296
vianarafaa <vianarafaa@gmail.com>
2026-04-06 08:57:03 -0300

    adiciona routes

---
+++ src/app/Routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router'
+import { LandingPage } from './components/LandingPage'
+import { Login } from './components/Login'
+import { Signup } from './components/Signup'
+import { ProtectedRoute } from './components/ProtectedRoute'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <LandingPage />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/signup',
+        element: <Signup />,
+    },
+])
