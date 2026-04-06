55195a20d540f113a19554dc2773e2b8af524f09
vianarafaa <vianarafaa@gmail.com>
2026-04-06 07:53:29 -0300

    ajusta rotas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
 import { LandingPage } from './components/LandingPage'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<LandingPage />} />
+            <Route
+                path="/"
+                element={
+                    <ProtectedRoute>
+                        <LandingPage />
+                    </ProtectedRoute>
+                }
+            />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
         </Routes>
