35ffa85df36a343b05e7b3c85a03c53b331ede98
vianarafaa <vianarafaa@gmail.com>
2026-04-06 07:46:07 -0300

    atualiza rotas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,14 @@
-import { RouterProvider } from 'react-router'
-import { router } from './routes'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
 import { LandingPage } from './components/LandingPage'
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App'
+import { HashRouter } from 'react-router-dom'
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <HashRouter>
+        <App />
+    </HashRouter>,
+)
