935afbe4a75d0c55a947f2aa78622bb380430b41
vianarafaa <vianarafaa@gmail.com>
2026-04-06 07:37:13 -0300

    atualiza rotas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,18 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import { RouterProvider } from 'react-router'
+import { router } from './routes'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './components/Login'
+import { Signup } from './components/Signup'
+import { LandingPage } from './components/LandingPage'
 
 export default function App() {
-  return <RouterProvider router={router} />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
